e2724405c160c3bf94d8a91d0c5ab7eeac850acc
Ludwig Ivan Comiso <comisoivan@gmail.com>
2023-01-21 19:57:21 +0800

    added loginpage

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Login } from "../features/authentication/login";
+
+export const LoginPage = () => {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+};
