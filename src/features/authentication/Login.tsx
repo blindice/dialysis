3142b903976fdb863408477516018d0b6a969fe0
Ludwig Ivan Comiso <comisoivan@gmail.com>
2023-01-23 07:05:07 +0800

    login soft design

--- src/features/authentication/Login.tsx
+++ src/features/authentication/Login.tsx
@@ -1,70 +1,94 @@
-import { useMutation } from "@tanstack/react-query";
-import { useForm } from "react-hook-form";
-import { useAtom } from "jotai";
+import { useMutation } from '@tanstack/react-query'
+import { useForm, Controller } from 'react-hook-form'
+import { useAtom } from 'jotai'
+import TextField from '@mui/material/TextField'
+import LoadingButton from '@mui/lab/LoadingButton'
 
-import { Account } from "./types";
-import { loginAsync } from "./api";
-import { Admin } from "../../types/Admin";
-import { userAtom } from "./atoms";
-import { toggleAtom } from "../../atoms";
+import { Account } from './types'
+import { loginAsync } from './api'
+import { Admin } from '../../types/Admin'
+import { userAtom } from './atoms'
+import { toggleAtom } from '../../atoms'
+import { SearchOffOutlined } from '@mui/icons-material'
 
 export const Login = () => {
-  const [, setUser] = useAtom(userAtom);
-  const [, setToggle] = useAtom(toggleAtom);
+  const [, setUser] = useAtom(userAtom)
+  const [, setToggle] = useAtom(toggleAtom)
 
   const {
-    register,
+    control,
     handleSubmit,
     formState: { errors },
-  } = useForm<Account>();
+  } = useForm<Account>()
 
   const mutation = useMutation<Admin, Error, Account, unknown>({
     mutationFn: ({ username, password }: Account) =>
       loginAsync({ username, password }),
     onSuccess: (data: Admin) => {
-      const { phone, ...rest } = data;
-      setUser(JSON.stringify(rest));
+      const { phone, ...rest } = data
+      setUser(JSON.stringify(rest))
     },
-  });
+  })
 
   return (
     <div
       style={{
-        display: "flex",
-        flexDirection: "column",
-        width: "15%",
-        border: "1px black solid",
-        padding: "0px 20px 20px 20px",
-        margin: "18% auto 0px",
+        display: 'flex',
+        flexDirection: 'column',
+        width: '15%',
+        border: '1px whitesmoke solid',
+        borderRadius: '10px',
+        padding: '0px 20px 20px 20px',
+        margin: '15% auto 0px',
+        fontFamily: 'Outfit',
+        boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 15px',
       }}
     >
       <h1>Login</h1>
-      <input
-        placeholder="Username"
-        {...register("username", { required: true })}
-      ></input>
-      <input
-        type="password"
-        placeholder="Password"
-        {...register("password", { required: true })}
-      ></input>
-      <button onClick={handleSubmit((data) => mutation.mutate(data))}>
-        Login
-      </button>
-      {/* <button
-        onClick={() => {
-          setToggle((prevState) => !prevState);
-        }}
+      <Controller
+        name="username"
+        control={control}
+        rules={{ required: true }}
+        render={({ field }) => (
+          <TextField
+            id="outlined-basic"
+            label="Username"
+            variant="outlined"
+            size="small"
+            style={{ marginBottom: '10px' }}
+            {...field}
+          />
+        )}
+      />
+      <Controller
+        name="password"
+        control={control}
+        rules={{ required: true }}
+        render={({ field }) => (
+          <TextField
+            id="outlined-basic"
+            label="Password"
+            variant="outlined"
+            size="small"
+            style={{ marginBottom: '10px' }}
+            {...field}
+          />
+        )}
+      />
+      <LoadingButton
+        variant="contained"
+        onClick={handleSubmit((data) => mutation.mutate(data))}
+        loading={mutation.isLoading}
       >
-        Create Admin
-      </button> */}
+        Login
+      </LoadingButton>
       {mutation.isError && (
         <h5>
-          {mutation.error.message.includes("(or no)")
-            ? "User Not Found"
-            : "Something went wrong!"}
+          {mutation.error.message.includes('(or no)')
+            ? 'User Not Found'
+            : 'Something went wrong!'}
         </h5>
       )}
     </div>
-  );
-};
+  )
+}
